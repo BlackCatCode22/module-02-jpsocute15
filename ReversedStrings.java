15e5a0bab40f1e9f6c2be447c7852a858bafb049
jpsocute15 <156354519+jpsocute15@users.noreply.github.com>
2024-02-03 12:14:50 -0800

    Update ReversedStrings.java

--- ReversedStrings.java
+++ ReversedStrings.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
 
-public class ReversedStrings {
+public class ReverseString {
     public static void main(String[] args) {
-
-        System.out.println("Welcome to My Strings program");
-
-        // Create a Scanner object to get a string from the user.
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n Please enter a string of characters...\n");
+
+        // Prompting user for input
+        System.out.print("Please enter your string: ");
         String userInput = scanner.nextLine();
 
-        // Output the line to reverse.
-        System.out.println("\n\nThe string to reverse is: " + userInput + "\n\n");
+        // Reversing the string
+        String reversedString = reverseString(userInput);
 
-        // Strings are a data structure meaning we can access individual characters like this
-        char aCharFromAString = userInput.charAt(3);
+        // Printing the reversed string
+        System.out.println("Reversed string: " + reversedString);
 
-        System.out.println("\n\n aCharFromAString is " + aCharFromAString);
+        scanner.close();
+    }
 
-        String reversedString = "";
-        for (int i = 0; i < userInput.length(); i++) {
-            System.out.println("\n  " + userInput.charAt(i) );
-            reversedString = userInput.charAt(i) + reversedString;
+    // Method to reverse a string
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
         }
-
-        System.out.println("\n reversed Str = " + reversedString);
-
+        return reversed.toString();
     }
 }
